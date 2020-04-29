d13de74320ed91cfeba4d242efe641325a3f98f1
riyos <yszk.tds@gmail.com>
2020-04-29 23:13:28 +0900

    スクリプトの実行ディレクトリによらずリンクを正しく貼れるよう修正

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DOTFILES=(.bashrc .vimrc .zshrc)
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 for file in ${DOTFILES[@]}
 do
-  ln -fnsv `pwd`/$file $HOME/$file
+  ln -fnsv ${SCRIPT_DIR}/$file $HOME/$file
 done
-
